b2bc51f7f2b1bcb1d7af6dd9e8c9ae46cd7e82c1
Zsuzsa Engelbrecht <zsuzsa.engelbrecht@gmail.com>
2020-09-09 22:56:17 +0200

    List activities in Client-App

---
+++ client-app/src/app/layout/App.tsx
@@ -0,0 +1,34 @@
+import React, { useState, useEffect } from 'react';
+import { Header, Icon, List } from 'semantic-ui-react';
+import axios from 'axios';
+import { IActivity } from '../models/activity';
+
+
+const App = () => {
+  const [activities, setActivities] = useState<IActivity[]>([])
+  
+  useEffect(() => {
+    axios
+    .get<IActivity[]>('https://localhost:44317/api/activities')
+    .then(response => {
+      setActivities(response.data)
+    });
+  }, []);
+
+  return (
+    <div>
+      <Header as='h2'>
+        <Icon name='users' />
+        <Header.Content>Reactivities</Header.Content>
+      </Header>
+      <List>
+        {activities.map((activity) => (
+          <List.Item key={activity.id}>{activity.title}</List.Item>
+        ))}
+      </List>
+    </div>
+  );
+  
+}
+
+export default App;
