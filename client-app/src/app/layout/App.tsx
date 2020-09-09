8fa7eabc6f42d98577c4c37db5fe5c37889075b7
Zsuzsa Engelbrecht <zsuzsa.engelbrecht@gmail.com>
2020-09-09 23:43:43 +0200

    Create a NavBar in Client-App

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,7 +1,8 @@
-import React, { useState, useEffect } from 'react';
-import { Header, Icon, List } from 'semantic-ui-react';
+import React, { useState, useEffect, Fragment } from 'react';
+import { List, Container } from 'semantic-ui-react';
 import axios from 'axios';
 import { IActivity } from '../models/activity';
+import { NavBar } from '../../features/nav/NavBar';
 
 
 const App = () => {
@@ -16,17 +17,17 @@ const App = () => {
   }, []);
 
   return (
-    <div>
-      <Header as='h2'>
-        <Icon name='users' />
-        <Header.Content>Reactivities</Header.Content>
-      </Header>
-      <List>
+    <Fragment>
+      <NavBar />
+      <Container style={{marginTop: '7em'}}>
+        <List>
         {activities.map((activity) => (
           <List.Item key={activity.id}>{activity.title}</List.Item>
         ))}
       </List>
-    </div>
+      </Container>
+      
+    </Fragment>
   );
   
 }
