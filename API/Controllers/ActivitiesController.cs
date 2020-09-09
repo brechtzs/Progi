811691dc86e532a0ce943ca83a84f6a5c642d142
Zsuzsa Engelbrecht <zsuzsa.engelbrecht@gmail.com>
2020-09-09 21:33:39 +0200

    Implement Edit Activity handler and controller method

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -28,12 +28,19 @@ namespace API.Controllers
         [HttpGet("{id}")]
         public async Task<ActionResult<Activity>> Details(Guid id)
         {
-            return await _mediator.Send(new Details.Query {Id = id});
+            return await _mediator.Send(new Details.Query { Id = id });
         }
 
         [HttpPost]
         public async Task<ActionResult<Unit>> Create(Create.Command command)
         {
+            return await _mediator.Send(command);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult<Unit>> Edit(Guid id, Edit.Command command)
+        {
+            command.Id = id;
             return await _mediator.Send(command);
         }
     }
