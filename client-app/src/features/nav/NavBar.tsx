c9b24ea5d63fc5aeb621ef1ffce9e190073ba558
Zsuzsa Engelbrecht <zsuzsa.engelbrecht@gmail.com>
2020-09-15 21:57:24 +0200

    Add Links and NavLinks

--- client-app/src/features/nav/NavBar.tsx
+++ client-app/src/features/nav/NavBar.tsx
@@ -1,5 +1,6 @@
 import { observer } from 'mobx-react-lite';
 import React, { useContext } from 'react';
+import { Link, NavLink } from 'react-router-dom';
 import { Menu, Container, Button } from 'semantic-ui-react';
 import ActivityStore from '../../app/stores/activityStore';
 
@@ -8,13 +9,19 @@ const NavBar: React.FC = () => {
     return (
         <Menu fixed='top' inverted>
             <Container>
-                <Menu.Item header>
-                    <img src="/assets/progi_logo.jpg" alt="logo" style={{marginRight: '10px'}}/>
+            <Menu.Item header as={NavLink} exact to='/'>
+                    <img 
+                        src="/assets/progi_logo.jpg" 
+                        alt="logo" 
+                        style={{marginRight: '10px'}}/>
                     Progi
                 </Menu.Item>
-                <Menu.Item name='Activities' />
+                <Menu.Item name='Activities' as={NavLink} to='/activities' />
                 <Menu.Item>
-                    <Button onClick={activityStore.openCreateForm} color='black' content='Create Activity'/>
+                    <Button 
+                        as={NavLink} to='/createActivity'  
+                        color='black' 
+                        content='Create Activity'/>
                 </Menu.Item>
             </Container>
         </Menu>
