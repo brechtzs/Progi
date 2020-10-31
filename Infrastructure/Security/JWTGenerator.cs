8a53681340cfabe1beddd8d6b765a5b52bb5bbfe
Zsuzsa Engelbrecht <zsuzsa.engelbrecht@gmail.com>
2020-10-31 11:32:42 +0100

    Create Infrastructure project and generate JWT token

---
+++ Infrastructure/Security/JWTGenerator.cs
@@ -0,0 +1,20 @@
+﻿using Application.Interfaces;
+using Domain;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Text;
+
+namespace Infrastructure.Security
+{
+    public class JWTGenerator : IJWTGenerator
+    {
+        public string CreateToken(AppUser user)
+        {
+            var claims = new List<Claim>
+            {
+                new Claim(JwtRegisteredClaimNames.NameId,)
+            }
+        }
+    }
+}
