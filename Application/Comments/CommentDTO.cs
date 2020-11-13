d6c080d05dd7ff5376894d04403b14f0d02aab6c
Zsuzsa Engelbrecht <zsuzsa.engelbrecht@gmail.com>
2020-11-13 12:58:11 +0100

    Add the CommentDTO and mapping

---
+++ Application/Comments/CommentDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Application.Activities
+{
+    public class CommentDTO
+    {
+        public Guid ID { get; set; }
+        public string Body { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public string Username { get; set; }
+        public string DisplayName { get; set; }
+        public string Image { get; set; }
+    }
+}
---
+++ Application/Comments/MappingProfile.cs
@@ -0,0 +1,18 @@
+﻿using Application.Activities;
+using AutoMapper;
+using Domain;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Application.Comments
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<Comment, CommentDTO>()
+                .ForMember(d => d.Username, o => o.)
+        }
+    }
+}
