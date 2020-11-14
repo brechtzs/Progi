aeb1931d48921e3c3a5c34bef61b6938dd8824cd
Zsuzsa Engelbrecht <zsuzsa.engelbrecht@gmail.com>
2020-11-14 23:22:03 +0100

    Add a custom value resolver for AutoMapper

--- Application/Activities/AttendeeDTO.cs
+++ Application/Activities/AttendeeDTO.cs
@@ -10,5 +10,6 @@ namespace Application.Activities
         public bool IsHost { get; set; }
         public string Username { get; set; }
         public string Image { get; set; }
+        public bool Following { get; set; }
     }
 }
--- Application/Activities/MappingProfile.cs
+++ Application/Activities/MappingProfile.cs
@@ -15,7 +15,8 @@ namespace Application.Activities
             CreateMap<UserActivity, AttendeeDTO>()
                 .ForMember(destination => destination.Username, option => option.MapFrom(source => source.AppUser.UserName))
                 .ForMember(d => d.DisplayName, o => o.MapFrom(s => s.AppUser.DisplayName))
-                .ForMember(d => d.Image, o => o.MapFrom(s => s.AppUser.Photos.FirstOrDefault(x => x.IsMain).Url));
+                .ForMember(d => d.Image, o => o.MapFrom(s => s.AppUser.Photos.FirstOrDefault(x => x.IsMain).Url))
+                .ForMember(d => d.Following, o => o.MapFrom<FollowingResolver>());
         }
     }
 }
