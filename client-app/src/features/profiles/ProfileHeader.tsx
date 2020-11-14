6d9528cf33523a00f7a7934fa24cb68783280638
Zsuzsa Engelbrecht <zsuzsa.engelbrecht@gmail.com>
2020-11-14 23:58:03 +0100

    Add follow/unfollow UI components

--- client-app/src/features/profiles/ProfileHeader.tsx
+++ client-app/src/features/profiles/ProfileHeader.tsx
@@ -4,10 +4,13 @@ import { IProfile } from '../../app/models/model';
 import { observer } from 'mobx-react-lite';
 
 interface IProps {
-  profile: IProfile
+  profile: IProfile,
+  isCurrentUser: boolean,
+  follow: (username: string) => void,
+  unfollow: (username: string) => void
 }
 
-const ProfileHeader: React.FC<IProps> = ({profile}) => {
+const ProfileHeader: React.FC<IProps> = ({profile, isCurrentUser, follow, unfollow}) => {
   return (
     <Segment>
       <Grid>
@@ -31,23 +34,25 @@ const ProfileHeader: React.FC<IProps> = ({profile}) => {
             <Statistic label='Following' value={profile.followingsCount} />
           </Statistic.Group>
           <Divider/>
+          {!isCurrentUser &&
           <Reveal animated='move'>
             <Reveal.Content visible style={{ width: '100%' }}>
               <Button
                 fluid
                 color='teal'
-                content='Following'
+                content={profile.following ? 'Following' : 'Not following'}
               />
             </Reveal.Content>
             <Reveal.Content hidden>
               <Button
                 fluid
                 basic
-                color={true ? 'red' : 'green'}
-                content={true ? 'Unfollow' : 'Follow'}
+                color={profile.following ? 'red' : 'green'}
+                content={profile.following ? 'Unfollow' : 'Follow'}
+                onClick={profile.following ? () => unfollow(profile.username) : () => follow(profile.username)}
               />
             </Reveal.Content>
-          </Reveal>
+          </Reveal>}
         </Grid.Column>
       </Grid>
     </Segment>
